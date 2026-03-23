62598f37a7291e71351f34f222968cd585674fcd
Ryann Valim <iryanngustavo@gmail.com>
2026-03-23 15:58:47 -0300

    initial commit

---
+++ Normalize.py
@@ -0,0 +1,13 @@
+"""
+Implementar uma classe capaz de normalizar dados conforme os métodos
+citados em sala:
+
+- MinMax Scaler;
+- Label Encoding;
+- One-Hot Encodig;
+- A classe (em Python) deve ser reaproveitável;
+- Deve-se implementar os métodos de reversão.
+
+Utilizar o arquivo "dados_normalizar.csv" para testar o código.
+"""
+
